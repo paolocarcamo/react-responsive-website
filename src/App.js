da1ba5009d291e5c4f678dd735eae0a4ae632b4e
Paolo Carcamo <Pogi@Paolo-Carcamo.local>
2021-04-09 12:23:29 -0700

    Finished webpage: Different sections & different routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/HomePage/Home';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+import Footer from './components/pages/Footer/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' />
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
